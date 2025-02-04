efa7234250e84b1b0f04f23bda58c3ee6ffe2be5
Shreyas A S <shreyas.as@techversantinfotech.com>
2025-02-04 11:54:32 +0530

    Make order searching more efficient

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#searchOrders').on('input', function() {
 		var searchText = $(this).val().toLowerCase();
 		$('.order-card').each(function() {
-			var orderText = $(this).text().toLowerCase();
+			var orderText = $(this).attr("data-order-id").toLowerCase();
 			$(this).toggle(orderText.includes(searchText));
 		});
 	});
