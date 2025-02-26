51dbd79720daea5522266f0d167e3d728fa6ca42
Shreyas A S <shreyas.as@techversantinfotech.com>
2025-02-26 18:07:33 +0530

    Add feature - focus search bar on ctrl+k and unfocus on esc

--- assets/js/header.js
+++ assets/js/header.js
@@ -21,3 +21,16 @@ function logOut() {
 		}
 	});
 }
+
+// Code to focus search bar when CTRL + K is pressed
+// and unfocus on pressing ESC
+$(window).keydown(function(event) {
+	if (event.ctrlKey && (event.key == 'k' || event.keyCode == 75)) {
+		event.preventDefault(); // Prevent the default browser behavior
+		$("#search").focus().select();
+	}
+
+	if (event.keyCode == 27) {
+		$("#search").blur();
+	}
+});
