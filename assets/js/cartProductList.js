cacc99f2953829959dde0d6cc941e00b3327f2f5
Shreyas A S <shreyas.as@techversantinfotech.com>
2025-03-24 14:31:30 +0530

    Add confirmation to product deletion from cart

--- assets/js/cartProductList.js
+++ assets/js/cartProductList.js
@@ -32,68 +32,78 @@ function editCartItem(containerId, productId, action) {
 		return;
 	}
 
-	// Disable button
-	clickedBtn.prop("disabled", true);
-
-	//Go on with execution if no alert is generated
-	$.ajax({
-		type: "POST",
-		url: "./components/cartManagement.cfc",
-		dataType: "json",
-		data: {
-			method: currentPage == "checkout.cfm" ? "modifyCheckout" : "modifyCart",
-			productId: productId,
-			action: action
-		},
-		success: function(response) {
-			const { quantity, price, actualPrice, totalActualPrice, totalPrice, totalTax } = response.data
-
-			if (response.success) {
-				if (quantity) {
-					// Quantity is present means product is still in cart
-					$(`#${containerId} span[name="price"]`).text(price.toFixed(2));
-					$(`#${containerId} span[name="actualPrice"]`).text(actualPrice.toFixed(2));
-					$(`#${containerId} input[name="quantity"]`).val(quantity).change();
-				} else {
-					// Quantity is not present means product is deleted
-					$(`#${containerId}`).remove();
-					if (currentPage == "cart.cfm") {
-						$("#cartCount").text(Number($("#cartCount").text())-1);
-					}
-				}
-
-				// Update total price and tax
-				$("#totalPrice").text(totalPrice.toFixed(2));
-				$("#totalActualPrice").text(totalActualPrice.toFixed(2));
-				$("#totalTax").text(totalTax.toFixed(2));
-
-				if (totalActualPrice.toFixed(2) == 0) {
-					// If total price is 0 (checkout is empty) then create alert
-					if (currentPage == "checkout.cfm") {
-						$("#productsNextBtn").prop("disabled", true);
-						$("#paymentSectionAccordionBtn").prop("disabled", true);
-						$("#accordionBody").append(`
-							<div class="d-flex flex-column align-items-center justify-content-center pt-4">
-								<div class="fs-5 text-secondary mb-3">Product List is Empty</div>
-								<a class="btn btn-primary" href="/">Shop Now</a>
-							</div>
-						`);
-					} else {
-						// If total price is 0 (cart is empty) then reload page
-						location.reload();
-					}
-				}
-			} else {
-				createAlert(containerId, "errorAlert", "Sorry. Unable to proceed. Try again.");
-			}
-		}
-	}).always(function() {
-		if (action == "decrement" && quantity == 2) {
-			// If button clicked was delete btn and old quantity = 2 (means current quantity is 1) then disable btn
+	Swal.fire({
+		icon: "warning",
+		title: "Delete product?",
+		showDenyButton: false,
+		showCancelButton: true,
+		confirmButtonText: "Ok",
+	}).then((result) => {
+		if (result.isConfirmed) {
+			// Disable button
 			clickedBtn.prop("disabled", true);
-		} else {
-			// Else Enable button back
-			clickedBtn.prop("disabled", false);
+
+			//Go on with execution
+			$.ajax({
+				type: "POST",
+				url: "./components/cartManagement.cfc",
+				dataType: "json",
+				data: {
+					method: currentPage == "checkout.cfm" ? "modifyCheckout" : "modifyCart",
+					productId: productId,
+					action: action
+				},
+				success: function(response) {
+					const { quantity, price, actualPrice, totalActualPrice, totalPrice, totalTax } = response.data
+
+					if (response.success) {
+						if (quantity) {
+							// Quantity is present means product is still in cart
+							$(`#${containerId} span[name="price"]`).text(price.toFixed(2));
+							$(`#${containerId} span[name="actualPrice"]`).text(actualPrice.toFixed(2));
+							$(`#${containerId} input[name="quantity"]`).val(quantity).change();
+						} else {
+							// Quantity is not present means product is deleted
+							$(`#${containerId}`).remove();
+							if (currentPage == "cart.cfm") {
+								$("#cartCount").text(Number($("#cartCount").text())-1);
+							}
+						}
+
+						// Update total price and tax
+						$("#totalPrice").text(totalPrice.toFixed(2));
+						$("#totalActualPrice").text(totalActualPrice.toFixed(2));
+						$("#totalTax").text(totalTax.toFixed(2));
+
+						if (totalActualPrice.toFixed(2) == 0) {
+							// If total price is 0 (checkout is empty) then create alert
+							if (currentPage == "checkout.cfm") {
+								$("#productsNextBtn").prop("disabled", true);
+								$("#paymentSectionAccordionBtn").prop("disabled", true);
+								$("#accordionBody").append(`
+									<div class="d-flex flex-column align-items-center justify-content-center pt-4">
+										<div class="fs-5 text-secondary mb-3">Product List is Empty</div>
+										<a class="btn btn-primary" href="/">Shop Now</a>
+									</div>
+								`);
+							} else {
+								// If total price is 0 (cart is empty) then reload page
+								location.reload();
+							}
+						}
+					} else {
+						createAlert(containerId, "errorAlert", "Sorry. Unable to proceed. Try again.");
+					}
+				}
+			}).always(function() {
+				if (action == "decrement" && quantity == 2) {
+					// If button clicked was delete btn and old quantity = 2 (means current quantity is 1) then disable btn
+					clickedBtn.prop("disabled", true);
+				} else {
+					// Else Enable button back
+					clickedBtn.prop("disabled", false);
+				}
+			});
 		}
 	});
 }
